4796c4ee110c1d03e3a24d82e699a407c5554ed3
Neel Bhanushali <neal.bhanushali@gmail.com>
2017-12-22 17:33:49 +0530

    user class file done

--- user.php
+++ user.php
@@ -2,29 +2,104 @@
 require_once 'db.php';
 
 class user {
-	public function __construct($id) {
+	private $table = 'definitions';
+
+	public function id($id) {
 		$db = new db();
 
-		$sql = "SELECT * FROM `definitions` WHERE id = :id";
+		$sql = "SELECT * FROM `{$this->table}` WHERE id = :id";
 
 		$query = $db->pdo->prepare($sql);
 
 		$query->execute([':id' => $id]);
 
-		if($query->rowCount()) {
-			$result = $query->fetch(PDO::FETCH_OBJ);
+		if($query->rowCount())
+			$this->result = $query->fetch(PDO::FETCH_OBJ);
+	}
 
-			foreach($result as $key => $value)
-				$this->$key = $value;
+	public function search($column, $param, $value) {
+		$db = new db();
+
+		$sql = "SELECT * FROM `{$this->table}` WHERE {$column} {$param} :value";
+
+		$query = $db->pdo->prepare($sql);
+
+		$query->execute([':value' => $value]);
+
+		if($query->rowCount()) {
+			if($query->rowCount() == 1)
+				$this->result = $query->fetch(PDO::FETCH_OBJ);
+			else 
+				while($result = $query->fetch(PDO::FETCH_OBJ))
+					$this->results[] = $result;
 		}
 	}
 
-	public function isEmpty() {
-		return empty(get_object_vars($this)) ? true : false;
+	public function insert($arr) {
+		$arr = json_decode(json_encode($arr));
+		$db = new db();
+
+		$sql = "INSERT INTO `{$this->table}`
+						VALUES(null, :subject, :word, :definition)";
+
+		$query = $db->pdo->prepare($sql);
+
+		$query->execute([
+			':subject' 		=> $arr->subject,
+			':word' 			=> $arr->word,
+			':definition' => $arr->definition
+		]);
+
+		if($query->rowCount())
+			$this->success = true;
 	}
+
+	public function update($column, $value, $wherecolumn, $whereparam, $wherevalue) {
+		$db = new db();
+
+		$sql = "UPDATE `{$this->table}` 
+						SET {$column} = :value
+						WHERE {$wherecolumn} {$whereparam} :wherevalue";
+
+		$query = $db->pdo->prepare($sql);
+
+		$query->execute([
+			':value' 			=> $value,
+			':wherevalue' => $wherevalue
+		]);
+
+		if($query->rowCount())
+			$this->success = true;
+	}
+
+
+	public function delete($column, $param, $value) {
+		$db = new db();
+
+		$sql = "DELETE FROM `{$this->table}` WHERE {$column} {$param} :value";
+
+		$query = $db->pdo->prepare($sql);
+
+		$query->execute([':value' => $value]);
+
+		if($query->rowCount())
+			$this->success = true;
+	}
+
 }
 
-$user = new user(2);
+// $user = new user();
+// $user->id(0);
+// $user->search('id', '=', '1');
+// $user->search('subject', 'LIKE', '%sad%');
+// $user->insert([
+// 	'subject'			=> 'mcc',
+// 	'word' 				=> 'jfj',
+// 	'definition' 	=> 'asdfg'
+// ]);
+// $user->update('subject', 'mcc', 'subject', '=', ':subject');
+// $user->delete('subject', '=', 'mcc');
 
-print_r($user);
+// print_r($user);
+
 ?>
